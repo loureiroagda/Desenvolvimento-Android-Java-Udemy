77f68111c78f894476c416c774ab4d9c4df1bedb
Agda Loureiro <loureiroagda@gmail.com>
2024-06-07 13:44:14 -0300

    Termino do app minhas anotações, contudo ainda tem que resolver o problema do floatbtn que não tá configurando direito

---
+++ app/src/minhasAnotacoes/java/com/example/appscursojavaudemy/AnotacaoPreferencias.java
@@ -0,0 +1,28 @@
+package com.example.appscursojavaudemy;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AnotacaoPreferencias {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private final String NOME_ARQUIVO = "nota.preferencia";
+    private final String CHAVE_NOME = "nome";
+
+    public AnotacaoPreferencias(Context c) {
+        this.context = c;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO, 0);
+        editor = preferences.edit();
+    }
+
+    public void salvarNota(String anotacao){
+        editor.putString(CHAVE_NOME, anotacao);
+        editor.commit();
+    }
+
+    public String recuperaNota(){
+        return preferences.getString(CHAVE_NOME, "");
+    }
+}
--- app/src/minhasAnotacoes/java/com/example/appscursojavaudemy/MainActivity.java
+++ app/src/minhasAnotacoes/java/com/example/appscursojavaudemy/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.appscursojavaudemy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.appscursojavaudemy.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    EditText nota;
+    AnotacaoPreferencias preferencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        nota = findViewById(R.id.nota);
+
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String texto = nota.getText().toString();
+
+                if(texto.equals(""))
+                {
+                    Snackbar.make(v, "Preencha a anotação", Snackbar.LENGTH_LONG).show();
+                }else
+                {
+                    preferencias.salvarNota(texto);
+                    Snackbar.make(v, "Nota salvo com sucesso", Snackbar.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+        //Recuperar a nota
+        String notaTXT = preferencias.recuperaNota();
+        if(!notaTXT.equals(""))
+        {
+            nota.setText(notaTXT);
+        }
     }
 }
